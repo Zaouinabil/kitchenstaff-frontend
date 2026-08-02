7bf0fc4f63c2d4e3b678e4e9ebef6fd92e6c112a
nabilzaoui <navil1228@gmail.com>
2026-08-02 12:10:05 +0200

    Add login form state

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  email = '';
+  password = '';
+  errorMessage = '';
 
+  onSubmit() {
+    console.log('Formulaire envoyé');
+
+    this.errorMessage = '';
+
+    if (!this.email.trim() || !this.password.trim()) {
+      this.errorMessage = 'Veuillez remplir l’email et le mot de passe.';
+      return;
+    }
+
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
 }
